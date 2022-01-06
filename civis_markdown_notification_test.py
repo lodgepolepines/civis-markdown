bb40eb2b08ca6da0e1952eefe876da0b3c9b2048
lodgepolepines <53026628+lodgepolepines@users.noreply.github.com>
2022-01-06 17:54:20 -0500

    mod

--- civis_markdown_notification_test.py
+++ civis_markdown_notification_test.py
@@ -2,27 +2,22 @@ import civis
 import json
 import os
 
+number = '40000K'
 
-def main():
-    number = '40000K'
+json_value_dict = {
+    'number': number
+}
 
-    json_value_dict = {
-        'number': number
-    }
-    post_json_run_output(json_value_dict)
+client = civis.APIClient()
+json_value_object = client.json_values.post(
+    json.dumps(json_value_dict),
+    name='email_outputs')
+client.scripts.post_python3_runs_outputs(
+    os.environ['CIVIS_JOB_ID'],
+    os.environ['CIVIS_RUN_ID'],
+    'JSONValue',
+    json_value_object.id)
 
-
-def post_json_run_output(json_value_dict):
-    client = civis.APIClient()
-    json_value_object = client.json_values.post(
-        json.dumps(json_value_dict),
-        name='email_outputs')
-    client.scripts.post_python3_runs_outputs(
-        os.environ['CIVIS_JOB_ID'],
-        os.environ['CIVIS_RUN_ID'],
-        'JSONValue',
-        json_value_object.id)
-
-
-if __name__ == '__main__':
-    main()
+print(os.environ['CIVIS_JOB_ID'])
+print(os.environ['CIVIS_RUN_ID'])
+print(json_value_object.id)
